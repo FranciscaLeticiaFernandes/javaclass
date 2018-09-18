ad158b41606607fafa3eab8190b55a4df34d7834
FranciscaLeticiaFernandes <lele.lf50@gmail.com>
2018-09-17 22:49:20 -0300

    aula decontrole de fluxo

---
+++ auladejava/promocaoAritimetica/src/promocaoAritimetica/Controles_de_fluxoDoPrograma.java
@@ -0,0 +1,30 @@
+package promocaoAritimetica;
+
+public class Controles_de_fluxoDoPrograma {
+		public static void main (String [] args) {
+			int idade = 20;
+			if (idade<=12) {
+				System.out.println("criança");
+			}
+			if (idade > 12 && idade <= 19 ) {
+				System.out.println("adolescente");
+			}
+			if (idade > 19 && idade <= 60) {
+				System.out.println("adulto");
+			}
+			if(idade > 60 ) {
+				System.out.println("idoso");
+				
+				
+		
+			}
+			
+		}
+}
+
+
+
+
+
+
+
---
+++ auladejava/promocaoAritimetica/src/promocaoAritimetica/clausula_If_else.java
@@ -0,0 +1,20 @@
+package promocaoAritimetica;
+
+public class clausula_If_else {
+	public static void main (String[] args) {
+		int idade=20;
+		if (idade<=12) {
+			System.out.println("crainça");
+		}
+		else if(idade <=19) {
+			System.out.println("adoslescente");
+		}
+		else if (idade<=60) {
+			System.out.println("adulto");
+		}
+		else {
+			System.out.println("idoso");
+		}
+	}
+
+}

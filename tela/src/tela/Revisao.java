f506e1cbb5c8d98685d61915398329ed87045446
FranciscaLeticiaFernandes <lele.lf50@gmail.com>
2018-11-27 16:38:37 -0200

    revisao de java

---
+++ tela/src/tela/Revisao.java
@@ -0,0 +1,16 @@
+package tela;
+
+public class Revisao {
+	public static void main (String args []) {
+		double d= 100.99;
+		int i = 100;
+		d=d*i;
+		
+		long x = 12345;
+		float pi=3.14F;
+		x= x*(long) pi;
+		x=(long) (x*pi);
+		System.out.println(x);
+	}
+
+}
